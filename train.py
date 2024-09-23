b871c6f46837990eb2a7ab34206c6e48ae6838a0
Khulood Abdullah <130120478+TheKhulood@users.noreply.github.com>
2024-09-23 04:25:01 +0300

    train.py

--- train.py
+++ train.py
@@ -6,7 +6,6 @@ from torch.utils.data import DataLoader
 import torch.nn as nn
 import torch.optim as optim
 
-import time
 from tqdm import tqdm
 from PIL import Image
 
@@ -14,37 +13,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 import json
 import argparse
+import os
 
 
 
 
 
-def get_input_args():
-    parser = argparse.ArgumentParser()
+# Argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument('--data_dir', type=str, default='./flowers/', help='the directory to pull information from')
+parser.add_argument('--arch', type=str, default='vgg16', help='architecture to utilize, choose one of these CNN models: vgg16 , alexnet')
+parser.add_argument('--json_file', type=str, default='cat_to_name.json', help='JSON file that maps the class values to other category names')
+parser.add_argument('--learning_rate', type=float, default=.001, help='the rate at which the model does its learning')
+parser.add_argument('--hidden_layer', type=int, default=4096, help='Dictates the hidden units for the hidden layer')
+parser.add_argument('--gpu', default='cuda', type=str, help='device where to run model: CPU vs. GPU')
+parser.add_argument('--epochs', type=int, default=5, help='number of cycles to train the model')
+parser.add_argument('--dropout', type=float, default=0.3, help='probability rate for dropouts')
+parser.add_argument('--save_dir', type=str, default='./checkpoint.pth', help='choose directory for saving')
 
-    parser.add_argument('--data_dir', type=str, default='./flowers/', help='the directory to pull information from')
-    parser.add_argument('--arch', type=str, default='vgg16', help='architecture to utilize, choose one from CNN models: vgg16, alexent')
-    parser.add_argument('--learning_rate', type=float, default=.001, help='the rate at which the model does its learning')
-    parser.add_argument('--hidden_layer', type=int, default=4096, help='Dictates the hidden units for the hidden layer')
-    parser.add_argument('--gpu', default='cuda', type=str, help='device where to run model: CPU vs. GPU')
-    parser.add_argument('--epochs', type=int, default=5, help='number of cycles to train the model')
-    parser.add_argument('--dropout', type=float, default=0.3, help='probability rate for dropouts')
-    parser.add_argument('--save_dir', type=str, default='./checkpoint.pth', help='choose directory for saving')
-
-    in_args = parser.parse_args()
-
-    return in_args
+in_args = parser.parse_args()
 
 
 
+data_dir = in_args.data_dir
+save_dir = in_args.save_dir
+json_file = in_args.json_file
+arch = in_args.arch
+lr = in_args.learning_rate
+hidden_layer = in_args.hidden_layer
+gpu = in_args.gpu
+epochs = in_args.epochs
+dropout = in_args.dropout
 
-data_dir = 'flowers'
+
+
+
 train_dir = data_dir + '/train'
 valid_dir = data_dir + '/valid'
 test_dir = data_dir + '/test'
 
 
-#Define your transforms for the training, validation, and testing sets
+#Define transforms for the training, validation, and testing sets
 mean =[0.485, 0.456, 0.406]
 std=[0.229, 0.224, 0.225]
 
@@ -62,7 +71,6 @@ data_transforms = {
     'validation': transforms.Compose([
         transforms.Resize(224),
         transforms.CenterCrop(224),
-        #transforms.RandomCrop(224),
         transforms.ToTensor(),
         transforms.Normalize(mean, std)
         ]),
@@ -71,7 +79,6 @@ data_transforms = {
     'testing':transforms.Compose([
         transforms.Resize(224),
         transforms.CenterCrop(224),
-        #transforms.RandomCrop(224),
         transforms.ToTensor(),
         transforms.Normalize(mean, std)
         ])
@@ -100,44 +107,45 @@ dataloaders = {
 }
 
 
+# Checking if JSON file exists
+if os.path.exists(json_file):
+    with open(json_file, 'r') as f:
+        cat_to_name = json.load(f)
+else:
+    print(f"Error: {json_file} not found.")
 
-
-
-#Imports cat_to_name.json file
-with open('cat_to_name.json', 'r') as f:
-    cat_to_name = json.load(f)
 
 
 
 
 
 # Building and training network
-
-device = torch.device("cuda" if in_args.gpu == "cuda" and torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if gpu == "cuda" and torch.cuda.is_available() else "cpu")
 print("device:{}".format(device))
 
-the_pretrained_model = models.in_args.arch(pretrained=True)
 
+the_pretrained_model = getattr(models, arch)(pretrained=True)
 
 
 for param in the_pretrained_model.parameters():
     param.requires_grad = False 
 
 
+input_features = the_pretrained_model.classifier[0].in_features
 
 the_pretrained_model.classifier = nn.Sequential(
-    nn.Linear(25088, in_args.hidden_layer),
-    nn.ReLU(), 
-    nn.Dropout(in_args.dropout),
-    nn.Linear(in_args.hidden_layer, 102)
-
+    nn.Linear(input_features, hidden_layer),
+    nn.ReLU(),
+    nn.Dropout(dropout),
+    nn.Linear(hidden_layer, 102),  # Assuming 102 output classes
+    nn.LogSoftmax(dim=1)
 )
 
 pretrained_model = the_pretrained_model.to(device)
 
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(pretrained_model.classifier.parameters(), lr=in_args.learning_rate)
+optimizer = optim.Adam(pretrained_model.classifier.parameters(), lr=lr)
 
 
 def train_one_epoch(model, dataloader, optimizer, device, criterion):
@@ -149,26 +157,19 @@ def train_one_epoch(model, dataloader, optimizer, device, criterion):
     for inputs, labels in tqdm(dataloader):
         inputs = inputs.to(device)
         labels = labels.to(device)
-
-
         optimizer.zero_grad()
-
-
         outputs = model(inputs.float())
         loss = criterion(outputs, labels)
-
-
         loss.backward()
         optimizer.step()
-
         running_loss += loss.item()
-
     return running_loss / len(dataloader)
 
 
 
 
 def validating_model(model, dataloader, device, criterion):
+    
     model.eval()
     running_loss = 0.0
     correct = 0
@@ -178,55 +179,35 @@ def validating_model(model, dataloader, device, criterion):
         for inputs, labels in tqdm(dataloader):
             inputs = inputs.to(device)
             labels = labels.to(device)
-            
             outputs = model(inputs)
             loss = criterion(outputs, labels)
-
             running_loss += loss.item()
-            
             _, predicted = torch.max(outputs, 1)
-
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
-    
     accuracy = correct / total
     return running_loss / len(dataloader), accuracy
 
 
 
 
-num_epochs = 5
 best_val_loss = float('inf')
-starting_time = time.time()
-
-for epoch in range(num_epochs):
-    
-
-    print(f"Epoch {epoch+1}/{num_epochs}")
-
-
+for epoch in range(epochs):
+    print(f"Epoch {epoch+1}/{epochs}")
     train_loss = train_one_epoch(pretrained_model, dataloaders['training'], optimizer, device, criterion)
-
-
     val_loss, val_accuracy = validating_model(pretrained_model, dataloaders['validation'], device, criterion)
-
-
-    print(f"Epoch number {epoch+1}/{num_epochs}, Training Loss: {train_loss:.2f}, Validation Loss: {val_loss:.2f}, Validation Accuracy: {val_accuracy:.2f}")
+    print(f"Epoch number {epoch+1}/{epochs}, Training Loss: {train_loss:.2f}, Validation Loss: {val_loss:.2f}, Validation Accuracy: {val_accuracy:.2f}")
 
 
     if val_loss < best_val_loss:
         best_val_loss = val_loss
-        torch.save(pretrained_model.state_dict(), 'trained_model.pth')
-
-time_end = time.time()
-print(f'Training time: {time_end - starting_time} seconds')
-
+        torch.save(pretrained_model.state_dict(), save_dir)
 
 
 
 
 # validation on the test set
-pretrained_model.load_state_dict(torch.load('trained_model.pth'))
+pretrained_model.load_state_dict(torch.load(save_dir))
 pretrained_model.eval()
 
 
@@ -240,11 +221,13 @@ print(f"Test Loss: {test_loss:.2f}, Test Accuracy: {test_accuracy:.2f}")
 pretrained_model.class_to_idx = datasets['training'].class_to_idx
 
 checkpoint = {
-    'arch': in_args.arch,
+    'arch': arch,
     'state_dict': pretrained_model.state_dict(),
     'class_to_idx': pretrained_model.class_to_idx,
     'optimizer_state_dict': optimizer.state_dict(),
-    'epoch': num_epochs,
-    'learning_rate': in_args.learning_rate
+    'epoch': epochs,
+    'hidden_layer': hidden_layer,
+    'learning_rate': lr,
+    'dropout': dropout
 }
-torch.save(checkpoint, f'checkpoint_{test_accuracy:.2f}.pth')
+torch.save(checkpoint, save_dir)
